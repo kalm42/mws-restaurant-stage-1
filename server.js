79c4cf334867c98e9a3ab9ab172d11bed64de930
Kyle Melton <kalm42@gmail.com>
2018-08-26 21:15:16 -0600

    fix: Server refactor.

--- server.js
+++ server.js
@@ -1,37 +1,17 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const http2 = require("spdy");
 const compression = require("compression");
-const expressStaticGzip = require("express-static-gzip");
 
 // Express app for serving our gulp'd files.
 const app = express();
 
+// Compress everything
 app.use(compression());
 
+// Hand over the static files
 app.use(express.static(path.join(__dirname, "build")));
 
-// app.use("/", expressStaticGzip(path.join(__dirname), { enableBrotli: true }));
-
-
-// app.get("/testing", function(req, res) {
-//   res.send("Hello World");
-// });
-
-const options = {
-  key: fs.readFileSync("./server.key"),
-  cert: fs.readFileSync("./server.crt")
-};
-
-// http2.createServer(options, app).listen(8000, err => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(`Listening on port 8000`);
-
-// });
-
+// Launch server
 app.set("port", 8000);
 const server = app.listen(app.get("port"), () => {
   console.log(`💻 Express running → PORT ${server.address().port}`);
